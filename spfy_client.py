4d846ae97677e966af9684d542c24490fb6a79f8
godwin-jg <89068702+godwin-jg@users.noreply.github.com>
2024-01-05 00:42:48 +0530

    Update spfy_client.py

--- spfy_client.py
+++ spfy_client.py
@@ -51,43 +51,4 @@ class SpotifyClient():
         refreshCall = Refresh()
         self.api_token = refreshCall.refresh()
         print(self.api_token)
-        # self.api_token = "BQCGYb20zMnsnNnq9kIQk--w9Y7Vz64w0TPOoc7ZHxFM7AYJSdclJ3Ak8d7T8vyeZTrH7h6wrD0_2cYbbGwUEphq5v8LuX2iUD-B19CkSTXzfdUydD9-RzjCGZ9s5_5Hwnx6VndioCmBoRKX5mzxTLdovHjesNI0BSobZZP-GKIX3HLTpJYfqeivhTWdqrF934YK2VQWkFKBimWqzu1wHm-nuSubqancqPvDuq4uwKCMCERahPAIkyE5hAGTcLwGFWOIFIo"
         
-    # def get_token():
-    #     client_id = 'a256f557f5724f81842eb24cb5c90bdd'
-    #     client_secret = '671b2ad12aa545e8a04f8d30e3206f4d'
-    #     redirect_uri = 'http%3A%2F%2Flocalhost%3A3000'
-
-    #     # Step 1: Authorization URL
-    #     authorization_url = (
-    #         "https://accounts.spotify.com/authorize?"
-    #         f"client_id={client_id}&"
-    #         "response_type=code&"
-    #         f"redirect_uri={redirect_uri}&"
-    #         "scope=playlist-modify-private playlist-modify-public"
-    #     )
-
-    #     print("Authorization URL:", authorization_url)
-
-    #     # Step 2: Redirect user to the authorization URL
-
-    #     # Step 3: After redirection, obtain the authorization code from the query parameters
-
-    #     # Step 4: Exchange authorization code for access token
-    #     authorization_code = ''
-
-    #     token_url = "https://accounts.spotify.com/api/token"
-    #     token_data = {
-    #         "grant_type": "authorization_code",
-    #         "code": authorization_code,
-    #         "redirect_uri": redirect_uri,
-    #         "client_id": client_id,
-    #         "client_secret": client_secret,
-    #     }
-
-    #     token_response = requests.post(token_url, data=token_data)
-    #     token_json = token_response.json()
-
-    #     # Step 5: Use the access token
-    #     access_token = token_json.get("access_token")
-    #     print("Access Token:", access_token)
